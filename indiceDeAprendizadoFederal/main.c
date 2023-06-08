f8de2b66c8e8c1f4139f78eef7e2026b297e86d5
Davi Dubeux <davidubeux10@gmail.com>
2023-06-07 21:27:33 -0300

    IAF1

--- indiceDeAprendizadoFederal/main.c
+++ indiceDeAprendizadoFederal/main.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #define e 2.71
 
 int main() {
 
-    long long int x1, x2, x3, y1, y2, y3;
-    int K1, K2, K3;
+    long long int x1, y1, x2, y2, x3, y3;
     float IAF1, IAF2, IAF3;
-    char desc1, desc2, desc3;
+    int k1, k2, k3;
 
-    scanf("%lld %lld %d\n", &x1, &y1, &K1);
-    scanf("%lld %lld %d\n", &x2, &y2, &K2);
-    scanf("%lld %lld %d\n", &x3, &y3, &K3);
+    scanf("%lld %lld %d", &x1, &y1, &k1);
+    scanf("%lld %lld %d", &x2, &y2, &k2);
+    scanf("%lld %lld %d", &x3, &y3, &k3);
 
-    IAF1 = ((x1^2)*e/y1);
-    IAF2 = ((x2^2)*e/y2);
-    IAF3 = ((x3^2)*e/y3);
+    if (IAF1 < 0) IAF1 = (-1)*(x1*x1*e/y1);
+    else IAF1 = x1*x1*e/y1;
+    if (IAF2 < 0) IAF2 = (-1)*(x2*x2*e/y2);
+    else IAF2 = x2*x2*e/y2;
+    if (IAF3 < 0) IAF3 = (-1)*(x3*x3*e/y3);
+    else IAF3 = x3*x3*e/y3;
 
-    if (IAF1 > 512 ) {
-        desc1 = 'Sem volta';
-    }
+    if (IAF1 > 512) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Sem volta\n", k1, IAF1);
+    else if (IAF1 >= 256) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Conserta impressoras\n", k1, IAF1);
+    else if (IAF1 >= 128)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Matemático Discreto\n", k1, IAF1);
+    else if (IAF1 >= 64) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Morador do Grad\n", k1, IAF1);
+    else if (IAF1 >= 32.5) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Iniciado\n", k1, IAF1);
+    else if (IAF1 < 32.5)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Calouro\n", k1, IAF1);
 
+    if (IAF2 > 512) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Sem volta\n", k2, IAF2);
+    else if (IAF2 >= 256) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Conserta impressoras\n", k2, IAF2);
+    else if (IAF2 >= 128)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Matemático Discreto\n", k2, IAF2);
+    else if (IAF2 >= 64) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Morador do Grad\n", k2, IAF2);
+    else if (IAF2 >= 32.5) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Iniciado\n", k2, IAF2);
+    else if (IAF2 < 32.5)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Calouro\n", k2, IAF2);
 
-    printf("O aluno de índice %d possui valor IAF igual a %.2f e a descrição de %s.", K1, IAF1, desc1);
+    if (IAF3 > 512) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Sem volta\n", k3, IAF3);
+    else if (IAF3 >= 256) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Conserta impressoras\n", k3, IAF3);
+    else if (IAF3 >= 128)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Matemático Discreto\n", k3, IAF3);
+    else if (IAF3 >= 64) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Morador do Grad\n", k3, IAF3);
+    else if (IAF3 >= 32.5) 
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Iniciado\n", k3, IAF3);
+    else if (IAF3 < 32.5)
+        printf("O aluno de índice %d possui o valor IAF igual a %.2f e a descrição de Calouro\n", k3, IAF3);
 
     return 0;
 }
