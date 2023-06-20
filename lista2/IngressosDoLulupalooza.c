a6421d0b5ca1692a7770cb59dbbad12f40aa8931
Davi Dubeux <davidubeux10@gmail.com>
2023-06-20 19:26:32 -0300

    IngressosDoLulupalooza5

--- lista2/IngressosDoLulupalooza.c
+++ lista2/IngressosDoLulupalooza.c
@@ -10,7 +10,7 @@ int main() {
 
     for(int i = 1; i <= n; i++){ //calcula o melhor canal
         
-        printf("----------\n");
+        // printf("----------\n");
         scanf("%d %d", &en1, &en2);
 
         min = 1;
@@ -39,14 +39,14 @@ int main() {
                 erroAtual = (x - seq3 >= 0 ? x - seq3 : seq3 - x);
             }
 
-            printf("erroAtual: %d  erroPassado: %d || seq1: %d  seq2: %d  seq3: %d  \n", erroAtual, erroPassado, seq1, seq2, seq3);
+            // printf("erroAtual: %d  erroPassado: %d || seq1: %d  seq2: %d  seq3: %d  \n", erroAtual, erroPassado, seq1, seq2, seq3);
 
             if(erroAtual<menorErro || (i==1 && min == 1)){
 
                 menorErro = erroAtual;
                 melhorCanal = i;
                 melhorMin = min;
-                printf("Menor erro: %d ", menorErro);
+                // printf("Menor erro: %d ", menorErro);
 
                 vip = 0;
                 
@@ -65,17 +65,17 @@ int main() {
                 while(temp>0){
                     vip += temp % 10;
                     temp /= 10;
-                    printf("vip: %d /", vip);
+                    // printf("vip: %d /", vip);
                 }
-                printf("\n");
+                // printf("\n");
             }
 
             min++;
         }
     }
 
-    printf("Menor erro de todos: %d\n", menorErro);
-    printf("Vip: %d\n", vip);
+    // printf("Menor erro de todos: %d\n", menorErro);
+    // printf("Vip: %d\n", vip);
     printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %d e sera chamado mais ou menos no minuto %d", melhorCanal, melhorMin);
     if(vip>10) printf(" e com o VIP garantido!!!\n");
     else printf(", mas o ingresso VIP não vai rolar :(\n");
